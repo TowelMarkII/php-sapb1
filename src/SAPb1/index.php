a95cf8f92fb72008cf51eda25869c9ca474b990e
Tonny Wildeman <tonny@towelmark2.nl>
2024-02-19 13:21:14 +0000

    adds index.php to include load the phar library

---
+++ src/SAPb1/index.php
@@ -0,0 +1,13 @@
+<?php 
+    // require the base classes
+    require_once 'Config.php';
+    require_once 'Query.php';
+    require_once 'Request.php';
+    require_once 'Response.php';
+    require_once 'SAPClient.php';
+    require_once 'SAPException.php';
+    require_once 'Service.php';
+
+    // require the filters
+    require_once 'Filters/index.php';
+?>

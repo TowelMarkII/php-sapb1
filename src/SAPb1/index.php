76fc4a5859053e7208fc4a61606a714df52494bc
Tonny Wildeman <tonny@towelmark2.nl>
2024-03-04 15:27:00 +0000

    adds php version detection and throws exception when lower than 8.1.0

--- src/SAPb1/index.php
+++ src/SAPb1/index.php
@@ -1,13 +1,15 @@
-<?php 
-    // require the base classes
-    require_once 'B1SVersion.php';
-    require_once 'Config.php';
-    require_once 'Query.php';
-    require_once 'Request.php';
-    require_once 'Response.php';
-    require_once 'SAPClient.php';    
-    require_once 'Service.php';
+<?
+if (version_compare("8.1.0", PHP_VERSION) >= 0)
+    throw new Exception("Version of php should be at least 8.1.0. You are running php version " . PHP_VERSION . ". The version is not supported by this release of php-sapb1 library.");
 
-    // require the filters
-    require_once 'Filters/index.php';
-?>
+// require the base classes
+require_once 'B1SVersion.php';
+require_once 'Config.php';
+require_once 'Query.php';
+require_once 'Request.php';
+require_once 'Response.php';
+require_once 'SAPClient.php';
+require_once 'Service.php';
+
+// require the filters
+require_once 'Filters/index.php';
